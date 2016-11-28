ed145175a97d89f909602b62e6c1db47be0a9f0e
Robin Deits <robin.deits@gmail.com>
2016-11-28 18:16:28 -0500

    speed up eval by 10x by fixing type instabilities
    remove notebook

---
+++ perf/runbenchmarks.jl
@@ -0,0 +1,13 @@
+using MultivariatePolynomials
+using BenchmarkTools
+
+let
+    @polyvar x y
+    p = x + y + 2x^2 + y^3
+
+    vars = [x, y]
+    vals = [1.0, 2.0]
+
+    println(@benchmark(($p)($vals, $vars)))
+    vals = MultivariatePolynomials.evalmap(MultivariatePolynomials.vars(p), vals, vars)
+end
