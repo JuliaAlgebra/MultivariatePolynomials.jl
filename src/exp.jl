6aeadca480062eb6d4fc7271c6db9d590b9984c7
Benoît Legat <benoit.legat@gmail.com>
2017-01-16 14:23:13 +0100

    Add NC measures

--- src/exp.jl
+++ src/exp.jl
@@ -1,6 +1,6 @@
 export expectation
 
-function dot(m::Measure, p::TermContainer)
+function _dot(m::AbstractMeasure, p::AbstractTermContainer, f)
     i = 1
     s = 0
     for t in p
@@ -10,13 +10,18 @@ function dot(m::Measure, p::TermContainer)
         if i > length(m.x)
             error("The polynomial $p has a monomial for which the expectation is not known in $m")
         end
-        s += m.a[i] * t.α
+        s += f(m.a[i], t.α)
         i += 1
     end
     s
 end
-dot(p::TermContainer, m::Measure) = dot(m, p)
-dot(m::Measure, p::PolyType) = dot(m, TermContainer(p))
-dot(p::PolyType, m::Measure) = dot(m, TermContainer(p))
-expectation(m::Measure, p::PolyType) = dot(m, p)
-expectation(p::PolyType, m::Measure) = dot(m, p)
+dot(m::AbstractMeasure, p::AbstractTermContainer) = _dot(m, p, (*))
+dot(p::AbstractTermContainer, m::AbstractMeasure) = _dot(m, p, (a, b) -> b * a)
+
+tcfor(::Measure) = TermContainer
+tcfor(::NCMeasure) = NCTermContainer
+dot(m::AbstractMeasure, p::PolyType) = dot(m, tcfor(m)(p))
+dot(p::PolyType, m::AbstractMeasure) = dot(tcfor(m)(p), m)
+
+expectation(m::AbstractMeasure, p::PolyType) = dot(m, p)
+expectation(p::PolyType, m::AbstractMeasure) = dot(p, m)
--- src/measure.jl
+++ src/measure.jl
@@ -1,39 +1,45 @@
-export Measure, zeta, ζ
+export Measure, NCMeasure, zeta, ζ
 
-type Measure{T}
-  a::Vector{T}
-  x::MonomialVector
+abstract AbstractMeasure
 
-  function Measure(a::Vector{T}, x::MonomialVector)
-    if length(a) != length(x)
-      error("There should be as many coefficient than monomials")
-    end
-    zeroidx = Int[]
-    for (i,α) in enumerate(a)
-      if iszero(α)
-        push!(zeroidx, i)
-      end
-    end
-    if !isempty(zeroidx)
-      isnz = ones(Bool, length(a))
-      isnz[zeroidx] = false
-      nzidx = find(isnz)
-      a = a[nzidx]
-      x = x[nzidx]
-    end
-    new(a, x)
-  end
-end
+for (MS, MV, PV) in [(:Measure, :MonomialVector, :PolyVar), (:NCMeasure, :NCMonomialVector, :NCPolyVar)]
+    @eval begin
+        type $MS{T} <: AbstractMeasure
+          a::Vector{T}
+          x::$MV
 
-Measure{T}(a::Vector{T}, x::MonomialVector) = Measure{T}(a, x)
-function Measure(a::Vector, x::Vector)
-    if length(a) != length(x)
-        error("There should be as many coefficient than monomials")
-    end
-    perm, X = sortmonovec(PolyVar, x)
-    Measure(a[perm], X)
-end
+          function $MS(a::Vector{T}, x::$MV)
+            if length(a) != length(x)
+              error("There should be as many coefficient than monomials")
+            end
+            zeroidx = Int[]
+            for (i,α) in enumerate(a)
+              if iszero(α)
+                push!(zeroidx, i)
+              end
+            end
+            if !isempty(zeroidx)
+              isnz = ones(Bool, length(a))
+              isnz[zeroidx] = false
+              nzidx = find(isnz)
+              a = a[nzidx]
+              x = x[nzidx]
+            end
+            new(a, x)
+          end
+        end
 
-function ζ{T}(v::Vector{T}, x::MonomialVector, varorder::Vector{PolyVar})
-  Measure(T[m(v, varorder) for m in x], x)
+        $MS{T}(a::Vector{T}, x::$MV) = $MS{T}(a, x)
+        function $MS(a::Vector, x::Vector)
+            if length(a) != length(x)
+                error("There should be as many coefficient than monomials")
+            end
+            perm, X = sortmonovec($PV, x)
+            $MS(a[perm], X)
+        end
+
+        function ζ{T}(v::Vector{T}, x::$MV, varorder::Vector{$PV})
+            $MS(T[m(v, varorder) for m in x], x)
+        end
+    end
 end
