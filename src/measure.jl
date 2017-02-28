f8a89596e6bb9178cfbf06c7da1dbb44ddd8dbd9
Benoît Legat <benoit.legat@gmail.com>
2017-02-28 19:16:23 +0100

    Fix #25

--- src/measure.jl
+++ src/measure.jl
@@ -33,7 +33,7 @@ function ζ{C, T}(v::Vector{T}, x::MonomialVector{C}, varorder::Vector{PolyVar{C
     Measure(T[m(v, varorder) for m in x], x)
 end
 
-function MatMesure{C, T}
+type MatMesure{C, T}
     Q::Vector{T}
     x::MonomialVector{C}
 end
