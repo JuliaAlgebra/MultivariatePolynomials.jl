82945a5fe62962b2a5d434c6a9a91c3944d04386
Benoît Legat <benoit.legat@gmail.com>
2017-05-01 16:36:09 -0400

    Add support for differentiation of fraction

--- test/diff.jl
+++ test/diff.jl
@@ -2,4 +2,6 @@
     @polyvar x y
     @test differentiate(MatPolynomial{true, Int}((i,j)->1, [x]), y) == 0
     @test differentiate(x*y + 3y^2 , [x, y]) == [y, x+6y]
+    @test differentiate(1 / x , [x, y]) == [-1/x^2, 0]
+    @test differentiate((x - y) / (x * y) , [x, y]) == [y^2 / (x * y)^2, -x^2 / (x * y)^2]
 end
