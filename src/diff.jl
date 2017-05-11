78c8098d63e5945945fbe2df29cb1d5c9df9c7fc
Joey Huchette <joehuchette@gmail.com>
2017-05-11 19:52:44 -0400

    Add method to compute higher-order derivatives (#35)
    * Add method to compute higher-order derivatives
    
    * Rewrite to use a recursive algorithm
    
    * Add tests
    
    * Fix domain error throwing in differentiate
    
    * Fix tests
    
    * Fix diff promote_op

--- src/diff.jl
+++ src/diff.jl
@@ -1,10 +1,14 @@
 # I do not use it but I import the function to add a method
 export differentiate
 
+differentiate(p::PolyVar, x)  = differentiate(Term(p), x)
+differentiate(p::Monomial, x) = differentiate(Term(p), x)
+
 function differentiate{C, T}(t::Term{C, T}, x::PolyVar{C})
     i = findfirst(vars(t), x)
     if i == 0 || t.x.z[i] == 0
-        zero(t)
+        S = Base.promote_op(*, T, Int)
+        zero(Term{C, S})
     else
         z = copy(t.x.z)
         z[i] -= 1
@@ -15,12 +19,12 @@ end
 function differentiate{C, T}(p::Polynomial{C, T}, x::PolyVar{C})
     # grlex order preserved
     i = findfirst(vars(p), x)
+    S = Base.promote_op(*, T, Int)
     if i == 0
-        zero(p)
+        zero(Polynomial{C, S})
     else
         keep = find([z[i] > 0 for z in p.x.Z])
         Z = [copy(p.x.Z[i]) for i in keep]
-        S = Base.promote_op(*, T, Int)
         a = Vector{S}(length(keep))
         for j in 1:length(Z)
             a[j] = p.a[keep[j]] * Z[j][i]
@@ -36,4 +40,21 @@ differentiate(p::RationalPoly, x::PolyVar) = (differentiate(p.num, x) * p.den - 
 
 function differentiate{C}(p::PolyType{C}, xs::Vector{PolyVar{C}})
     [differentiate(p, x) for x in xs]
+end
+
+# In Julia v0.5, Base.promote_op returns Any for PolyVar, Monomial and MatPolynomial
+_diff_promote_op(S, T) = Base.promote_op(differentiate, S, T)
+_diff_promote_op{C}(::Type{PolyVar{C}}, ::Type{PolyVar{C}}) = Term{true, Int}
+_diff_promote_op{C}(::Type{Monomial{C}}, ::Type{PolyVar{C}}) = Term{true, Int}
+_diff_promote_op{C, T}(::Type{MatPolynomial{C, T}}, ::Type{PolyVar{C}}) = Polynomial{true, Base.promote_op(*, T, Int)}
+
+function differentiate(p::PolyType, x, deg::Int)
+    if deg < 0
+        throw(DomainError())
+    elseif deg == 0
+        # Need the conversion with promote_op to be type stable for PolyVar, Monomial and MatPolynomial
+        return _diff_promote_op(typeof(p), typeof(x))(p)
+    else
+        return differentiate(differentiate(p, x), x, deg-1)
+    end
 end
--- test/diff.jl
+++ test/diff.jl
@@ -1,7 +1,24 @@
 @testset "Differentiation" begin
     @polyvar x y
+    @test differentiate(true*x+true*x^2, y) == 0
+    @inferred differentiate(true*x+true*x^2, y)
     @test differentiate(MatPolynomial{true, Int}((i,j)->1, [x]), y) == 0
     @test differentiate(x*y + 3y^2 , [x, y]) == [y, x+6y]
+    @inferred differentiate(x*y + 3y^2 , [x, y])
     @test differentiate(1 / x , [x, y]) == [-1/x^2, 0]
     @test differentiate((x - y) / (x * y) , [x, y]) == [y^2 / (x * y)^2, -x^2 / (x * y)^2]
+    p = x^2-y+x*y
+    @test differentiate(p, x, 2) == 2
+    @test differentiate(p, x, 0) === p
+    @test_throws DomainError differentiate(p, x, -1)
+    @test differentiate(x, x, 1) == 1
+    @inferred differentiate(x, x, 0)
+    @inferred differentiate(x, x, 1)
+    @test differentiate(x*y^4, y, 3) == 24x*y
+    @inferred differentiate(x*y^4, y, 3)
+    @test differentiate(x*y^4, y, 0) == x*y^4
+    @test differentiate(2x^2, x, 2) == 4
+    @inferred differentiate(2x^2, x, 2)
+    @test differentiate(MatPolynomial{true, Int}((i,j)->1, [x, y]), y, 1) == 2x + 2y
+    @inferred differentiate(MatPolynomial{true, Int}((i,j)->1, [x, y]), y, 0)
 end
