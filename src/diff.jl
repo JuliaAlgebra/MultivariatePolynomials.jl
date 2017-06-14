fffe7a8701aea376dcd80e07b020ae458561c0a9
Benoît Legat <benoit.legat@gmail.com>
2017-06-14 13:41:47 +0200

    Fix #38

--- src/diff.jl
+++ src/diff.jl
@@ -38,17 +38,28 @@ differentiate(p::MatPolynomial, x) = differentiate(Polynomial(p), x)
 
 differentiate(p::RationalPoly, x::PolyVar) = (differentiate(p.num, x) * p.den - p.num * differentiate(p.den, x)) / p.den^2
 
-function differentiate{C}(p::PolyType{C}, xs::Vector{PolyVar{C}})
+# even if I annotate with ::Array{_diff_promote_op(T, PolyVar{C}), N+1}, it cannot detect the type since it seems to be unable to determine the dimension N+1 :(
+function differentiate{N, C, T<:PolyType{C}}(ps::AbstractArray{T, N}, xs::Union{AbstractVector{PolyVar{C}}, Tuple})
+    qs = Array{_diff_promote_op(T, PolyVar{C}), N+1}(length(xs), size(ps)...)
+    for (i, x) in enumerate(xs)
+        for j in linearindices(ps)
+            J = ind2sub(ps, j)
+            qs[i, J...] = differentiate(ps[J...], x)
+        end
+    end
+    qs
+end
+function differentiate{C}(p::PolyType{C}, xs::Union{AbstractVector{PolyVar{C}}, Tuple})
     [differentiate(p, x) for x in xs]
 end
 
 # In Julia v0.5, Base.promote_op returns Any for PolyVar, Monomial and MatPolynomial
+# Even on Julia v0.6 and Polynomial, Base.promote_op returns Any...
 _diff_promote_op(S, T) = Base.promote_op(differentiate, S, T)
-_diff_promote_op{C}(::Type{PolyVar{C}}, ::Type{PolyVar{C}}) = Term{true, Int}
-_diff_promote_op{C}(::Type{Monomial{C}}, ::Type{PolyVar{C}}) = Term{true, Int}
-_diff_promote_op{C, T}(::Type{MatPolynomial{C, T}}, ::Type{PolyVar{C}}) = Polynomial{true, Base.promote_op(*, T, Int)}
+_diff_promote_op{C}(::Union{Type{PolyVar{C}}, Type{Monomial{C}}}, ::Type{PolyVar{C}}) = Term{true, Int}
+_diff_promote_op{C, T}(::Union{Type{Polynomial{C, T}}, Type{MatPolynomial{C, T}}}, ::Type{PolyVar{C}}) = Polynomial{true, Base.promote_op(*, T, Int)}
 
-function differentiate(p::PolyType, x, deg::Int)
+function differentiate{T<:PolyType}(p::Union{T, AbstractArray{T}}, x, deg::Int)
     if deg < 0
         throw(DomainError())
     elseif deg == 0
