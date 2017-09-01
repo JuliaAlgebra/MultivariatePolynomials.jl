d1c389d4f2a772969875ae195aef38fd603d13ea
Benoît Legat <benoit.legat@gmail.com>
2017-09-02 00:01:58 +0200

    Improve testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,23 @@ const MP = MultivariatePolynomials
 
 include("utils.jl")
 
-import DynamicPolynomials
-Mod = DynamicPolynomials
-include("commutativetests.jl")
-include("noncommutativetests.jl")
+# Taken from JuMP/test/solvers.jl
+function try_import(name::Symbol)
+    try
+        @eval import $name
+        return true
+    catch e
+        return false
+    end
+end
 
-import TypedPolynomials
-Mod = TypedPolynomials
-include("commutativetests.jl")
+if try_import(:DynamicPolynomials)
+    Mod = DynamicPolynomials
+    include("commutativetests.jl")
+    include("noncommutativetests.jl")
+end
+
+if try_import(:TypedPolynomials)
+    Mod = TypedPolynomials
+    include("commutativetests.jl")
+end
