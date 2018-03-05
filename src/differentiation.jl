488d9063177966b7573ed91193825cc8be191168
Robin Deits <robin.deits@gmail.com>
2018-03-05 10:52:04 -0500

    docs

--- src/differentiation.jl
+++ src/differentiation.jl
@@ -2,15 +2,20 @@
 export differentiate
 
 """
-    differentiate(p::AbstractPolynomialLike, v::AbstractVariable, deg::Int=1)
+    differentiate(p::AbstractPolynomialLike, v::AbstractVariable, deg::Union{Int, Val}=1)
 
 Differentiate `deg` times the polynomial `p` by the variable `v`.
 
-    differentiate(p::AbstractPolynomialLike, vs, deg::Int=1)
 
-Differentiate `deg` times the polynomial `p` by the variables of the vector or tuple of variable `vs` and return an array of dimension `deg`.
+    differentiate(p::AbstractPolynomialLike, vs, deg::Union{Int, Val}=1)
 
-    differentiate(p::AbstractArray{<:AbstractPolynomialLike, N}, vs, deg::Int=1) where N
+Differentiate `deg` times the polynomial `p` by the variables of the vector or
+tuple of variable `vs` and return an array of dimension `deg`. It is recommended
+to pass `deg` as a `Val` instance when the degree is known at compile time, e.g.
+`differentiate(p, v, Val{2}())` instead of `differentiate(p, x, 2)`, as this
+will help the compiler infer the return type.
+
+    differentiate(p::AbstractArray{<:AbstractPolynomialLike, N}, vs, deg::Union{Int, Val}=1) where N
 
 Differentiate the polynomials in `p` by the variables of the vector or tuple of variable `vs` and return an array of dimension `N+deg`.
 
@@ -19,6 +24,7 @@ Differentiate the polynomials in `p` by the variables of the vector or tuple of 
 ```julia
 p = 3x^2*y + x + 2y + 1
 differentiate(p, x) # should return 6xy + 1
+differentiate(p, x, Val{1}()) # equivalent to the above
 differentiate(p, (x, y)) # should return [6xy+1, 3x^2+1]
 ```
 """
