787e3872dab4adaeff99b2b292ddc9854e7d9f0f
Sascha Timme <sascha.timme@gmail.com>
2019-06-25 08:50:31 +0200

    Add documentation for new differentiate behaviour

--- src/differentiation.jl
+++ src/differentiation.jl
@@ -17,6 +17,8 @@ will help the compiler infer the return type.
     differentiate(p::AbstractArray{<:AbstractPolynomialLike, N}, vs, deg::Union{Int, Val}=1) where N
 
 Differentiate the polynomials in `p` by the variables of the vector or tuple of variable `vs` and return an array of dimension `N+deg`.
+If `p` is an `AbstractVector` this returns the Jacobian of `p` where the i-th row containts the partial
+derivaties of `p[i]`.
 
 ### Examples
 
@@ -25,6 +27,7 @@ p = 3x^2*y + x + 2y + 1
 differentiate(p, x) # should return 6xy + 1
 differentiate(p, x, Val{1}()) # equivalent to the above
 differentiate(p, (x, y)) # should return [6xy+1, 3x^2+1]
+differentiate( [x^2+y, z^2+4x], [x, y, z]) # should return [2x 1 0; 4 0 2z]
 ```
 """
 function differentiate end
