ee470eabebff58f86f5856341c8b78f94f12358f
Marcelo Forets <mforets@gmail.com>
2020-02-25 22:27:30 -0300

    Update MultivariatePolynomials.jl

--- src/MultivariatePolynomials.jl
+++ src/MultivariatePolynomials.jl
@@ -47,7 +47,7 @@ Abstract type for a monomial, i.e. a product of variables elevated to a nonnegat
 abstract type AbstractMonomial <: AbstractMonomialLike end
 
 """
-    AbstractTerm{T} <: AbstractTerm{T}
+    AbstractTerm{T} <: AbstractTermLike{T}
 
 Abstract type for a term of coefficient type `T`, i.e. the product between a value of type `T` and a monomial.
 """
