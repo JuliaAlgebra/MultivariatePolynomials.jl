ddf199b53042a6918a52b9dcf2f72065239b9331
Benoît Legat <benoit.legat@gmail.com>
2020-05-29 12:57:17 +0200

    Fix differentiation of zero polynomial

--- src/differentiation.jl
+++ src/differentiation.jl
@@ -37,7 +37,15 @@ differentiate(α::T, v::AbstractVariable) where T = zero(T)
 differentiate(v1::AbstractVariable, v2::AbstractVariable) = v1 == v2 ? 1 : 0
 differentiate(t::AbstractTermLike, v::AbstractVariable) = coefficient(t) * differentiate(monomial(t), v)
 # The polynomial function will take care of removing the zeros
-differentiate(p::APL, v::AbstractVariable) = polynomial!(differentiate.(terms(p), v), SortedState())
+function differentiate(p::APL, v::AbstractVariable)
+    if iszero(p)
+        # As `terms(p)` is empty, `differentiate.(terms(p), v)` gives `Any[]`.
+        T = typeof(differentiate(one(termtype(p)), v))
+        polynomial!(T[], SortedUniqState())
+    else
+        polynomial!(differentiate.(terms(p), v), SortedState())
+    end
+end
 differentiate(p::RationalPoly, v::AbstractVariable) = (differentiate(p.num, v) * p.den - p.num * differentiate(p.den, v)) / p.den^2
 
 const ARPL = Union{APL, RationalPoly}
