6315b505f214f63b29baa689ffc3c22463463049
Benoît Legat <benoit.legat@gmail.com>
2021-04-10 23:40:31 +0200

    Add a term method with coefficient and monomial (#149)
    * Add a term method with coefficient and monomial
    
    * Fixes
    
    * Update DynamicPolynomials and TypedPolynomials lower bounds

--- src/term.jl
+++ src/term.jl
@@ -1,6 +1,19 @@
 export constantterm, term, termtype, zeroterm, coefficient, monomial
 
 """
+    term(coef, mono::AbstractMonomialLike)
+
+Returns a term with coefficient `coef` and monomial `mono`. There are two key
+difference between this and `coef * mono`:
+
+* `term(coef, mono)` does not copy `coef` and `mono` so modifying this term
+  with MutableArithmetics may modifying the input of this function.
+  To avoid this, call `term(MA.copy_if_mutable(coef), MA.copy_if_mutable(mono))`
+  where `MA = MutableArithmetics`.
+* Suppose that `coef = (x + 1)` and `mono = x^2`, `coef * mono` gives the polynomial
+  with integer coefficients `x^3 + x^2` which `term(x + 1, x^2)` gives a term
+  with polynomial coefficient `x + 1`.
+
     term(p::AbstractPolynomialLike)
 
 Converts the polynomial `p` to a term.
@@ -8,6 +21,8 @@ When applied on a polynomial, it throws an `InexactError` if it has more than on
 When applied to a term, it is the identity and does not copy it.
 When applied to a monomial, it create a term of type `AbstractTerm{Int}`.
 """
+function term end
+term(coef, var::AbstractVariable) = term(coef, monomial(var))
 term(p::APL) = convert(termtype(p), p)
 
 """
