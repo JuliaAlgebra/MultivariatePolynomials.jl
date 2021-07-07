f0c8d6f20a016dd5a595830d78be68e408c70e5c
Benoît Legat <benoit.legat@gmail.com>
2021-07-07 16:44:04 -0400

    Fix promotion with nonconcrete types

--- test/promote.jl
+++ test/promote.jl
@@ -22,6 +22,68 @@
     @test Y[1] == x + 3y
     @test Y[2] == 2x + 4y
     @test dot(X, [1 2; 3 4] * X) == x^2 + 5x*y + 4y^2
+
+    function _t(a, b, T)
+        @test typeof(@inferred vcat(a, b)) == Vector{T}
+        @test typeof(@inferred vcat(b, a)) == Vector{T}
+    end
+
+    function __test(a, PT, TT, MT)
+        @test typeof(a) == Vector{MT(Int)}
+        _t(a, 1, Any)
+        _t(a, x, MT(Int))
+        _t(a, x^2, MT(Int))
+        _t(a, 1x, TT(Int))
+        _t(a, 1.0x, TT())
+        _t(a, x + y, PT(Int))
+        _t(a, 1.0x + y, PT())
+    end
+
+    function _test(a, af, TT)
+        __test(a, apl, TT, TT)
+        # `x isa APL{Int}` so here we don't have `Float64`:
+        pt() = apl()
+        pt(T) = apl()
+        tt() = TT()
+        tt(T) = TT()
+        __test(af, pt, tt, tt)
+        @test typeof(af) == Vector{TT()}
+        _t(af, a, TT())
+    end
+
+    apl() = MP.APL
+    apl(T::Type) = MP.APL{T}
+    p = [i == 1 ? x + y : x for i in 1:2]
+    pf = [i == 1 ? 1.0x + y : x for i in 1:2]
+    _test(p, pf, apl)
+
+    atl() = MP.AbstractTermLike
+    atl(T::Type) = MP.AbstractTermLike{T}
+    t = [i == 1 ? 2x : x for i in 1:2]
+    tf = [i == 1 ? 2.0x : x for i in 1:2]
+    _test(t, tf, atl)
+    _t(p, t, apl(Int))
+    _t(pf, t, apl())
+    _t(p, tf, apl())
+    _t(pf, tf, apl())
+
+    __pt = Base.promote_typejoin(typeof(x + 2), typeof(x + 2.0))
+    _pt() = __pt
+    _pt(::Type) = __pt
+    __test([i == 1 ? x + 2 : x + 2.0 for i in 1:2], _pt, _pt, _pt)
+
+    __tt = Base.promote_typejoin(typeof(2x), typeof(2.0x))
+    _tt() = __tt
+    _tt(::Type) = __tt
+    __test([i == 1 ? 2x : 2.0x for i in 1:2], _pt, _tt, _tt)
+
+    aml(args...) = MP.AbstractMonomialLike
+    a = [i == 1 ? x^2 : x for i in 1:2]
+    __test(a, apl, atl, aml)
+    _t(a, t, atl(Int))
+    _t(a, tf, atl())
+    _t(a, p, apl(Int))
+    _t(a, pf, apl())
 end
 
 struct A end
--- test/tmp.jl
+++
@@ -1,9 +0,0 @@
-function mapexponents_test()
-    @polyvar x y
-    a = x^2
-    b = x * y
-    MultivariatePolynomials.mapexponents!(+, a, b)
-    dump(a)
-    @test variables(a) == variables(b)
-end
-mapexponents_test()
