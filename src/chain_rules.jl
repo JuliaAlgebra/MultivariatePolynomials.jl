917816c8bd89f6ac89ed4b7e94c640507a34be7d
Benoît Legat <benoit.legat@gmail.com>
2022-04-07 18:48:53 -0400

    Add chain rule for differentiate

--- src/chain_rules.jl
+++ src/chain_rules.jl
@@ -1,3 +1,13 @@
 import ChainRulesCore
 
 ChainRulesCore.@scalar_rule +(x::APL, y::APL) (true, true)
+function ChainRulesCore.frule((_, Δp, _), ::typeof(differentiate), p, x)
+    return differentiate(p, x), differentiate(Δp, x)
+end
+function ChainRulesCore.rrule(::typeof(differentiate), p, x)
+    dpdx = differentiate(p, x)
+    function pullback(Δdpdx)
+        return ChainRulesCore.NoTangent(), x * differentiate(x * Δdpdx, x), ChainRulesCore.NoTangent()
+    end
+    return dpdx, pullback
+end
--- test/chain_rules.jl
+++ test/chain_rules.jl
@@ -1,12 +1,39 @@
-using Test
+using LinearAlgebra, Test
 using ChainRulesCore
+
+function test_chain_rule(dot, op, args, Δin, Δout)
+    output = op(args...)
+    foutput, fΔout = ChainRulesCore.frule((NoTangent(), Δin...), op, args...)
+    @test output == foutput
+    routput, pullback = ChainRulesCore.rrule(op, args...)
+    @test output == routput
+    rΔin = pullback(Δout)
+    @test rΔin[1] == NoTangent()
+    @test dot(Δin, rΔin[2:end]) ≈ dot(fΔout, Δout)
+end
 
 @testset "ChainRulesCore" begin
     Mod.@polyvar x y
-    p = x + y
-    q = x - y
+    p = 1.1x + y
+    q = -0.1x - y
     output, pullback = ChainRulesCore.rrule(+, p, q)
-    @test output == 2x
+    @test output == 1.0x
     @test pullback(2) == (NoTangent(), 2, 2)
     @test pullback(x + 3) == (NoTangent(), x + 3, x + 3)
+    output, pullback = ChainRulesCore.rrule(differentiate, p, x)
+    @test output == 1.1
+    @test pullback(q) == (NoTangent(), -0.2x^2 - x*y, NoTangent())
+    test_chain_rule(dot, +, (p, q), (q, p), p)
+    test_chain_rule(dot, +, (p, q), (p, q), q)
+    function _dot(p, q)
+        monos = monomials(p + q)
+        return dot(coefficient.(p, monos), coefficient.(q, monos))
+    end
+    function _dot(px::Tuple{<:AbstractPolynomial,NoTangent}, qx::Tuple{<:AbstractPolynomial,NoTangent})
+        return _dot(px[1], qx[1])
+    end
+    test_chain_rule(_dot, differentiate, (p, x), (q, NoTangent()), p)
+    test_chain_rule(_dot, differentiate, (p, x), (q, NoTangent()), differentiate(p, x))
+    test_chain_rule(_dot, differentiate, (p, x), (q, NoTangent()), differentiate(q, x))
+    test_chain_rule(_dot, differentiate, (p, x), (p * q, NoTangent()), p)
 end
