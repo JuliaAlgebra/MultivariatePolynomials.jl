7cd2cb3a0b096ca107397e98fddef258155e176a
Valter Schütz <valterschutz@protonmail.com>
2022-07-29 13:00:17 +0200

    Fixed typo

--- src/polynomial.jl
+++ src/polynomial.jl
@@ -252,7 +252,7 @@ Returns the maximal total degree of the monomials of `p`, i.e. `maximum(degree, 
 Returns the maximal degree of the monomials of `p` in the variable `v`, i.e. `maximum(degree.(terms(p), v))`.
 
 ### Examples
-Calling `maxdegree` on on ``4x^2y + xy + 2x`` should return 3, `maxdegree(4x^2y + xy + 2x, x)` should return 2 and  `maxdegree(4x^2y + xy + 2x, y)` should return 1.
+Calling `maxdegree` on ``4x^2y + xy + 2x`` should return 3, `maxdegree(4x^2y + xy + 2x, x)` should return 2 and  `maxdegree(4x^2y + xy + 2x, y)` should return 1.
 """
 function maxdegree(X::AbstractVector{<:AbstractTermLike}, args::Vararg{Any,N}) where {N}
     return mapreduce(t -> degree(t, args...), max, X, init=0)
