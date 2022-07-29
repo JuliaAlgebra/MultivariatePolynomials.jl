550de40c483a4afa7f14946deba661b69e0acb86
Valter Schütz <valterschutz@protonmail.com>
2022-07-29 12:59:03 +0200

    Fixed typo

--- src/polynomial.jl
+++ src/polynomial.jl
@@ -272,7 +272,7 @@ Returns the extremal total degrees of the monomials of `p`, i.e. `(mindegree(p),
 Returns the extremal degrees of the monomials of `p` in the variable `v`, i.e. `(mindegree(p, v), maxdegree(p, v))`.
 
 ### Examples
-Calling `extdegree` on on ``4x^2y + xy + 2x`` should return `(1, 3)`, `extdegree(4x^2y + xy + 2x, x)` should return `(1, 2)` and  `maxdegree(4x^2y + xy + 2x, y)` should return `(0, 1)`.
+Calling `extdegree` on ``4x^2y + xy + 2x`` should return `(1, 3)`, `extdegree(4x^2y + xy + 2x, x)` should return `(1, 2)` and  `maxdegree(4x^2y + xy + 2x, y)` should return `(0, 1)`.
 """
 function extdegree(p::Union{AbstractPolynomialLike, AbstractVector{<:AbstractTermLike}}, args...)
     (mindegree(p, args...), maxdegree(p, args...))
