8b13b7704df3fdc318d4e1d5bab4d319770f0c39
Benoît Legat <benoit.legat@gmail.com>
2023-05-23 10:46:34 +0200

    Add compare_monomials

--- src/sequences.jl
+++ src/sequences.jl
@@ -8,23 +8,24 @@ function merge_sorted!(
     result,
     v1::AbstractArray,
     v2::AbstractArray,
-    isless,
+    compare,
     combine,
     filter = x -> !iszero(x),
 )
-    i = 1
-    i1 = 1
-    i2 = 1
-    while i1 <= length(v1) && i2 <= length(v2)
+    i = firstindex(result)
+    i1 = firstindex(v1)
+    i2 = firstindex(v2)
+    while i1 <= lastindex(v1) && i2 <= lastindex(v2)
         x1 = v1[i1]
         x2 = v2[i2]
-        if isless(x1, x2)
+        Δ = compare(x1, x2)
+        if Δ < 0
             if filter(x1)
                 result[i] = x1
                 i += 1
             end
             i1 += 1
-        elseif isless(x2, x1)
+        elseif Δ > 0
             if filter(x2)
                 result[i] = x2
                 i += 1
@@ -40,13 +41,13 @@ function merge_sorted!(
             i2 += 1
         end
     end
-    for j in i1:length(v1)
+    for j in i1:lastindex(v1)
         if filter(v1[j])
             result[i] = v1[j]
             i += 1
         end
     end
-    for j in i2:length(v2)
+    for j in i2:lastindex(v2)
         if filter(v2[j])
             result[i] = v2[j]
             i += 1
@@ -59,13 +60,13 @@ end
 function merge_sorted(
     v1::AbstractArray,
     v2::AbstractArray,
-    isless = Base.isless,
+    compare,
     combine = Base.:+,
     filter = x -> !iszero(x),
 )
     T = MA.promote_operation(combine, eltype(v1), eltype(v2))
     result = Vector{T}(undef, length(v1) + length(v2))
-    return merge_sorted!(result, v1, v2, isless, combine, filter)
+    return merge_sorted!(result, v1, v2, compare, combine, filter)
 end
 
 end
