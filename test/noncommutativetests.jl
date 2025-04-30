414a4d594a71a1f01b416b2bbb0056008ad154f5
Benoît Legat <benoit.legat@gmail.com>
2025-04-30 16:07:29 +0200

    Move implementation tests to subdirectories

--- test/noncommutativetests.jl
+++ test/noncommutativetests.jl
@@ -1,3 +1,3 @@
-include("ncmonomial.jl")
-include("ncoperators.jl")
-include("nccomparison.jl")
+for file in readdir(joinpath(@__DIR__, "noncommutative"))
+    include(joinpath(@__DIR__, "noncommutative", file))
+end
