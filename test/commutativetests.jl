9fc5d7d0086206e82bd83002dd75a63bd6e7c0e2
Benoît Legat <benoit.legat@gmail.com>
2025-05-01 08:00:38 +0200

    Move implementation tests to subdirectories (#314)
    * Move implementation tests to subdirectories
    
    * Add indendent back

--- test/commutativetests.jl
+++ test/commutativetests.jl
@@ -1,30 +1,6 @@
-include("mutable_arithmetics.jl")
-
-include("zip.jl")
-include("variable.jl")
-include("monomial.jl")
-include("term.jl")
-include("monomial_vector.jl")
-include("polynomial.jl")
-include("det.jl")
-
-include("rational.jl")
-isdefined(Mod, Symbol("@complex_polyvar")) && include("complex.jl")
-
-include("promote.jl")
-include("hash.jl")
-include("norm.jl")
-
-include("algebra.jl")
-include("comparison.jl")
-
-include("substitution.jl")
-include("differentiation.jl")
-include("division.jl")
-include("gcd.jl")
-include("chain_rules.jl")
-
-include("show.jl")
-
-include("example1.jl")
-include("example2.jl")
+for file in readdir(joinpath(@__DIR__, "commutative"))
+    if file == "complex.jl" && !isdefined(Mod, Symbol("@complex_polyvar"))
+        continue
+    end
+    include(joinpath(@__DIR__, "commutative", file))
+end
