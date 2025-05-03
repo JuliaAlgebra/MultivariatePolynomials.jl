8da25869f8988c34e56254376cf2cb832a233d39
Benoît Legat <benoit.legat@gmail.com>
2025-05-03 16:58:11 +0200

    Add MA tests back

--- test/commutativetests.jl
+++ test/commutativetests.jl
@@ -2,8 +2,5 @@ for file in readdir(joinpath(@__DIR__, "commutative"))
     if file == "complex.jl" && !isdefined(Mod, Symbol("@complex_polyvar"))
         continue
     end
-    if file == "mutable_arithmetics.jl"
-        continue
-    end
     include(joinpath(@__DIR__, "commutative", file))
 end
