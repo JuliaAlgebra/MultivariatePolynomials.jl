26e48c267a5d5d2ec8ce9a354e63a185993b6b28
Benoît Legat <benoit.legat@gmail.com>
2025-05-04 13:32:13 +0200

    Avoid including independent twice

--- test/commutativetests.jl
+++ test/commutativetests.jl
@@ -1,4 +1,8 @@
 for file in readdir(joinpath(@__DIR__, "commutative"))
+    if file == "independent.jl"
+        # It's included by `gcd.jl`
+        continue
+    end
     if file == "complex.jl" && !isdefined(Mod, Symbol("@complex_polyvar"))
         continue
     end
