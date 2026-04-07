b79a84bb4993bfc4b7af7b13826845b32165f769
Benoît Legat <benoit.legat@gmail.com>
2026-04-07 08:24:05 +0200

    Fix inference issue for is_commutative (#355)
    * Fix inference issue for is_commutative
    
    * fix
    
    * Add tests
    
    * Fix format

--- test/commutative/variable.jl
+++ test/commutative/variable.jl
@@ -13,6 +13,10 @@ import MultivariatePolynomials:
         _is_comm_test(x^2)
         _is_comm_test(2x^2)
         _is_comm_test(sum(y))
+        # Test that `is_commutative` is constant-folded for variable types,
+        # so that branches on it are eliminated at compile time.
+        _comm_branch(::Type{V}) where {V} = MP.is_commutative(V) ? Int : String
+        @test @inferred(_comm_branch(typeof(variables(x * y[1])))) === Int
     end
     @testset "polyvar macro index set" begin
         Mod.@polyvar x y z
