bb3e6847dbd16312a3949380b3853b292373c281
pekpuglia <pepuglia@gmail.com>
2026-08-25 19:01:26 -0300

    add simple latexification for polynomials

--- ext/MultivariatePolynomialsLatexifyExt.jl
+++ ext/MultivariatePolynomialsLatexifyExt.jl
@@ -4,4 +4,12 @@ using Latexify
 @latexrecipe function f(m::AbstractMonomial)
     return Expr(:call, :*, (:($(Symbol(effvar)) ^ $e) for (effvar, e) = zip(effective_variables(m), exponents(m)))...)
 end
+
+@latexrecipe function f(t::AbstractTerm)
+           return Expr(:call, :*, coefficient(t), monomial(t))
+       end
+
+@latexrecipe function f(p::Polynomial)
+    return Expr(:call, :+, terms(p)...)
+end
 end
