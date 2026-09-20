eb61dc624743631367b6584cf9eddcf387d145cf
pekpuglia <pepuglia@gmail.com>
2026-09-20 12:12:22 -0300

    format test as well

--- test/commutative/latexify.jl
+++ test/commutative/latexify.jl
@@ -1,13 +1,12 @@
 using Latexify
 
 @testset "Latexify Extension" begin
-    
     Mod.@polyvar y[1:10]
 
     #test no error is thrown
     @test latexify(y[1]) isa Latexify.LaTeXStrings.LaTeXString #variable
     @test latexify(prod(y)) isa Latexify.LaTeXStrings.LaTeXString #monomial
     @test latexify(sqrt(3) * prod(y)) isa Latexify.LaTeXStrings.LaTeXString #term
-    @test latexify(sum(sqrt(i) * y[i]^i for i=1:10)) isa Latexify.LaTeXStrings.LaTeXString #polynomial
+    @test latexify(sum(sqrt(i) * y[i]^i for i in 1:10)) isa
+          Latexify.LaTeXStrings.LaTeXString #polynomial
 end
-
