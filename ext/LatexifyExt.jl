70dc5d3d5d20db429b4203e8585eabfa5f51fbca
pekpuglia <63191877+pekpuglia@users.noreply.github.com>
2026-09-21 03:14:02 -0300

    Latexify integration (#365)
    * add project.toml
    
    * add latexrecipe for monomials
    
    * fix deps
    
    * add simple latexification for polynomials
    
    * simplify name according to general convention, use abstract types for everything
    
    * add some tests
    
    * fix exponents vs effective_variables issue, polish monomial
    
    * remove unit coefficient
    
    * fix variable naming
    
    * format code
    
    * format test as well

---
+++ ext/LatexifyExt.jl
@@ -0,0 +1,31 @@
+module LatexifyExt
+using MultivariatePolynomials
+using Latexify
+@latexrecipe function f(m::AbstractMonomialLike)
+    operation := :*
+    mult_symbol --> ""
+    index --> :subscript
+    vars = [Meta.parse(string(v)) for v in variables(m)]
+    exps = exponents(m)
+    factors = (
+        (e == 1) ? var : :($(var) ^ $e) for
+        (var, e) in zip(vars, exps) if e != 0
+    )
+    return Expr(:call, :*, factors...)
+end
+
+@latexrecipe function f(t::AbstractTermLike)
+    operation := :*
+    mult_symbol --> ""
+    coeff = coefficient(t)
+    if coeff == one(typeof(coeff))
+        return Expr(:call, :*, monomial(t))
+    else
+        return Expr(:call, :*, coefficient(t), monomial(t))
+    end
+end
+
+@latexrecipe function f(p::AbstractPolynomialLike)
+    return Expr(:call, :+, terms(p)...)
+end
+end
